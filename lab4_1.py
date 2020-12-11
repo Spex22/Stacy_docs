bfb2c9bb76191c485372b7befca04324ec33b8bb
Spex22 <75040198+Spex22@users.noreply.github.com>
2020-12-11 16:09:16 +0300

    Add files via upload

---
+++ lab4_1.py
@@ -0,0 +1,29 @@
+#Скопировать из файла F1 в файл F2 строки, начиная с К до К+5.
+# Подсчитать количество гласных букв в F2.
+from collections import Counter
+import collections
+
+k = int(input('Введите номер строки:  '))
+nline = []
+gl = ['а','о','у','ы','и','я','е','ё','ю']
+sum = 0
+
+for i in range(6):
+    nline.append(k)
+    k += 1
+
+print('Вывод номеров строк: ' + str(nline))
+
+file = open('for_l4.txt')
+new_file = open('for_l.txt','w')
+
+for n, line in enumerate(file):
+    if (n+1) in nline:
+        new_file.write(line)
+
+file.close()
+new_file.close()
+
+
+
+
