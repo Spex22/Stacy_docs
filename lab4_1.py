db248c58a01c7c975148f048b4c41bcf4412664b
Spex22 <75040198+Spex22@users.noreply.github.com>
2020-12-14 18:29:27 +0300

    Update lab4_1.py

--- lab4_1.py
+++ lab4_1.py
@@ -1,11 +1,9 @@
 #Скопировать из файла F1 в файл F2 строки, начиная с К до К+5.
 # Подсчитать количество гласных букв в F2.
 from collections import Counter
-import collections
 
 k = int(input('Введите номер строки:  '))
 nline = []
-gl = ['а','о','у','ы','и','я','е','ё','ю']
 sum = 0
 
 for i in range(6):
@@ -17,6 +15,7 @@ print('Вывод номеров строк: ' + str(nline))
 file = open('for_l4.txt')
 new_file = open('for_l.txt','w')
 
+#добавление строк с к=того элемента
 for n, line in enumerate(file):
     if (n+1) in nline:
         new_file.write(line)
@@ -24,8 +23,14 @@ for n, line in enumerate(file):
 file.close()
 new_file.close()
 
+#Вывод содержимого файла
 with open('for_l.txt', 'r') as f:
     print(f.read())
-
-
-
+    
+#gодсчет гласных в новом файле
+f = open('for_l.txt','r')
+counter = Counter(f.read())
+print(counter)
+sum = (counter['а'] + counter['о'] + counter['у'] + counter['ю'] + counter['и'] + counter['ы'] +counter['е'] + counter['э'] + counter['ё'] + counter['я'])
+print(sum)
+f.close()
